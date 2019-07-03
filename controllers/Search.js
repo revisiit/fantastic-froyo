6d9223e310acc00956d4de799988b5a0f598f1da
Sanjay <sivasanjay7@gmail.com>
2019-07-03 14:36:49 +0530

    Updated controllers/Search.js

--- controllers/Search.js
+++ controllers/Search.js
@@ -3,9 +3,21 @@ const { Package } = require('../models')
 exports.searchItem = (req, res) => {
   var item = req.body.item
   Package.find({
-    name: {
-      $regex: new RegExp(item),
+    $text: {
+      $search: item,
     },
+
+    // **** EFFICIENT SORTING METHOD ****
+
+    // }, {
+    //     score: {
+    //         $meta: "textScore"
+    //     }
+    // })
+    // .sort({
+    //     score: {
+    //         $meta: "textScore"
+    //     }
   })
     .select({
       _id: 0,
@@ -16,4 +28,21 @@ exports.searchItem = (req, res) => {
     .catch(err => {
       res.send(err)
     })
+
+  // **** PARTIAL SEARCHING ****
+
+  // Package.find({
+  //         name: {
+  //             $regex: new RegExp(item),
+  //         }
+  //     })
+  //     .select({
+  //         _id: 0,
+  //     })
+  //     .then(data => {
+  //         res.send(data)
+  //     })
+  //     .catch(err => {
+  //         res.send(err)
+  //     })
 }
