fd139a48a9b7476fca5b0c7ba7e3bd0bbdfcc99f
Sanjay <sivasanjay7@gmail.com>
2019-07-14 01:25:14 +0530

    Added Template for WelcomeMail

---
+++ controllers/email.js
@@ -0,0 +1,41 @@
+const nodemailer = require('nodemailer')
+const email = require('email-templates')
+
+// Storing the sender email details
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'haventcreatedyet@gmail.com',
+    pass: 'password',
+  },
+})
+
+module.exports = {
+  sendWelcomeMail: function(user) {
+    const Email = new email({
+      transport: transporter,
+      send: true,
+      preview: false,
+    })
+
+    // .send contains the message to be sent to the registered user and send the mail using nodemailer
+
+    Email.send({
+      template: 'Welcome',
+      message: {
+        to: 'sivasanjay7@gmail.com',
+        subject: 'Welcome to Visiit',
+      },
+      locals: {
+        name: user.first_name,
+      },
+    })
+      .then(info => {
+        console.log('Email sent:' + info.response)
+      })
+      .catch(err => {
+        console.log('Error in sending :' + err)
+      })
+  },
+}
